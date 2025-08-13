fec7a1ab925718c00a0ec99da889e926d68c56ae
Anil <c4.anil.gupta@gmail.com>
2025-08-13 00:28:40 -0400

    updated

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,19 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    if(req.session.authorization){
+        let token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access", (err, user) => {
+            if(!err){
+                req.user = user;
+                next()
+            }else{
+                return req.statusCode(403).json({message: "User not authenticated"});
+            }
+        })
+    }else{
+        return req.statusCode(403).json({message: "User not logged in!"});
+    }
 });
  
 const PORT =5000;
@@ -20,3 +33,5 @@ app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
+
+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
