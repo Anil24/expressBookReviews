72f0908e4b5c55c80123a502105b6451971da543
anil <c4.anil.gupta@gmail.com>
2025-08-16 09:04:54 -0400

    added async get book

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const url = 'https://raw.githubusercontent.com/Anil24/expressBookReviews/refs/heads/main/final_project/router/booksdb.js';
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -24,23 +25,36 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   //Write your code here
-  return res.status(300).send(JSON.stringify(books, null, 4));
+  try{
+    const response = await axios.get(url);
+    return res.status(300).send(JSON.stringify(books, null, 4));
+  }catch (error){
+    return res.status(500).json({messsage: "Error in fatching a book"});
+  }
+  
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
-  let isbn = req.params.isbn;
-  let book = Object.entries(books).filter(([key,book]) => 
-    book.isbn === isbn
-  )
+  try{
+    const bookslist = await axios.get(url);
+    let isbn = req.params.isbn;
+    let book = Object.entries(bookslist).filter(([key,book]) => 
+        book.isbn === isbn
+    )
 
-  if(book.length > 0){
-    return res.status(300).send(Object.fromEntries(book));
-  }else{
-    return res.status(300).json({message: "No book found by ISBN"});
+    console.log("test", bookslist, book);
+
+    if(book.length > 0){
+        return res.status(300).send(Object.fromEntries(book));
+    }else{
+        return res.status(300).json({message: "No book found by ISBN"});
+    }
+  }catch (error){
+    return res.status(500).json({messsage: "Error in fatching a book"});
   }
  });
   
